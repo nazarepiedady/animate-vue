0da3db2f5c58b0a1f07b0503169b3a60eb65f9d5
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-31 11:12:31 +0100

    add style to card in simple view

--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -10,5 +10,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.card {
+  width: 6.5em;
+  height: 6.5em;
+  display: block;
+  border-radius: 1%;
+  margin: 0 auto 0 auto;
+  background-color: #16c0b0;
+  box-shadow: 0.08em 0.03em 0.4em #ababab;
+}
 </style>
