8bd158a8e0fca8c75a47c52bafe315d22da440b0
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-31 09:47:29 +0100

    add transition component to drawer div

--- src/views/Drawer.vue
+++ src/views/Drawer.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
     <button @click="isOpen = !isOpen">My Profile</button>
-    <div v-if="isOpen" class="drawer">
-      <img src="../assets/avatar.png" alt="avater" />
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+      :css="false"
+    >
+      <div v-if="isOpen" class="drawer">
+        <img src="../assets/avatar.png" alt="avater" />
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </transition>
   </div>
 </template>
 
