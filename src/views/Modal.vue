1ebb7a05aaf3312f9842b37e04daa87f4a34e808
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-31 08:27:13 +0100

    add name to modal view after being renamed

---
+++ src/views/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal">
+    <button @click="toggleModal">Open</button>
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <button @click="toggleModal">Close</button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    },
+  },
+}
+</script>
+
+<style>
+</style>
