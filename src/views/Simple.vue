3843bf8c2325a9c4c6699748f171912f280badf9
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-03-31 11:33:37 +0100

    animate card using gsap platform

--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -10,8 +10,19 @@ import gsap from 'gsap'
 export default {
   name: 'Simple',
   methods: {
-    beforeEnter(el) {},
-    enter(el, done) {},
+    beforeEnter(el) {
+      el.style.opacity = 0
+      el.style.transform = 'scale(0,0)'
+    },
+    enter(el, done) {
+      gsap.to(el, {
+        duration: 1,
+        opacity: 1,
+        scale: 1,
+        ease: 'bounce.out',
+        onComplete: done,
+      })
+    },
   },
 }
 </script>
